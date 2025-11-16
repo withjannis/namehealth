2a9f23fe9f0dc94d1217efedc3e234c3717f8a1d
withjannis <125810509+withjannis@users.noreply.github.com>
2025-11-16 16:09:15 +0100

    Added docstrings and fixed unused variables

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,25 +1,23 @@
-import fastapi
-import fastapi.responses
+"""
+Backend main application file.
+"""
 
 import time
-
 import json
+
+import fastapi
+import fastapi.responses
 
 import db
 
 app = fastapi.FastAPI()
 
 
-def generate_html_response():
-    with open("src/frontend/view.html", encoding="utf-8") as f:
-        html = f.read()
-    return fastapi.responses.HTMLResponse(content=html, status_code=200)
-
-
 @app.get(
     "/meta/site.webmanifest", response_class=fastapi.responses.FileResponse
 )
 async def site_webmanifest():
+    """Serve site.webmanifest file."""
     return fastapi.responses.FileResponse("src/frontend/site.webmanifest")
 
 
@@ -27,6 +25,7 @@ async def site_webmanifest():
     "/meta/{icon_name}.png", response_class=fastapi.responses.FileResponse
 )
 async def favicon(icon_name: str):
+    """Serve favicon and touch icons."""
     if icon_name in [
         "favicon-16x16",
         "favicon-32x32",
@@ -41,11 +40,15 @@ async def favicon(icon_name: str):
 
 @app.get("/", response_class=fastapi.responses.HTMLResponse)
 async def view_domain():
-    return generate_html_response()
+    """Serve the main HTML view."""
+    with open("src/frontend/view.html", encoding="utf-8") as f:
+        html = f.read()
+    return fastapi.responses.HTMLResponse(content=html, status_code=200)
 
 
 @app.get("/api/v1/domains", response_class=fastapi.responses.JSONResponse)
 async def list_domain():
+    """List all monitored domains."""
     domain_info = db.list_items("config#domain#")
     print(domain_info)
 
@@ -61,6 +64,15 @@ async def list_domain():
     response_class=fastapi.responses.JSONResponse,
 )
 async def get_domain(domain: str, record_type: str):
+    """Get information about domain
+
+    Args:
+        domain (str): domain name
+        record_type (str): DNS record type
+
+    Returns:
+        JSONResponse: JSON with domain information
+    """
     domain_info = db.get_latest_item(domain + "#" + record_type)
     print(domain_info)
     time.sleep(0.3)
@@ -77,8 +89,16 @@ async def get_domain(domain: str, record_type: str):
     )
 
 
-@app.head("/api/v1/domain/{domain}/{record_type}")
-async def head_domain(domain: str, record_type: str):
+@app.head("/api/v1/domain/{domain}/{_}")
+async def head_domain(domain: str, _: str):
+    """HEAD request to check if domain record exists.
+    Args:
+        domain (str): domain name
+        record_type (str): DNS record type
+    Returns:
+        Response: 200 if exists, 204 if not
+    """
+
     domain_info = db.get_item(domain)
     print(domain_info)
     if domain_info is None:
