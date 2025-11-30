9bcf4ce9a8566e37b13e0f3c7586a7df59ae06c3
withjannis <125810509+withjannis@users.noreply.github.com>
2025-11-30 19:29:56 +0100

    main.rs: added chaos monkey

--- src/collector/main.rs
+++ src/collector/main.rs
@@ -2,16 +2,15 @@ use std::{ vec, str::FromStr };
 
 use std::net::{ IpAddr, SocketAddr };
 
+use domain::base::ToName;
 use domain::base::{
     Message,
     MessageBuilder,
     Name,
     Rtype,
-    ToName,
     Record,
     ParsedName,
     message::RecordSection,
-    wire::ParseError,
 };
 
 use domain::net::client::{ dgram, dgram_stream, multi_stream, stream };
@@ -20,9 +19,20 @@ use domain::net::client::request::{ ComposeRequest, RequestMessage, SendRequest 
 
 use domain::rdata::{ A, Ns };
 
+use rand;
+
 use std::time::Duration;
 
-#[derive(Debug)]
+async fn chaos_monkey(info: &str) -> bool {
+    let x = rand::random::<u8>();
+    if 10 > x {
+        println!("⚠️ {}: trigger error {}", info, x);
+        return true;
+    };
+    return false;
+}
+
+#[derive(Debug, Clone)]
 struct NhQuery {
     na: Name<Vec<u8>>,
     rt: Rtype,
@@ -63,13 +73,16 @@ impl NhResolver {
 
     async fn _sctn_rcrds(
         &self,
-        a: Result<&RecordSection<'_, bytes::Bytes>, &ParseError>,
+        a: &RecordSection<'_, bytes::Bytes>,
         b: &mut Vec<Record<ParsedName<bytes::Bytes>, A>>
-    ) -> () {
-        let answ_sec = match a {
-            Ok(o) => o.limit_to_in::<A>(),
-            Err(e) => panic!("{e}"),
-        };
+    ) -> Result<(), ()> {
+
+        // fuzzing my code so trigger errors on purpose
+        if chaos_monkey("_sctn_rcrds").await {
+            return Err(());
+        }
+
+        let answ_sec = a.limit_to_in::<A>();
 
         for rcrd_res in answ_sec {
             let rcrd = match rcrd_res {
@@ -82,153 +95,134 @@ impl NhResolver {
 
             b.push(rcrd);
         }
-        return ();
+        return Ok(());
     }
 
-    async fn _ns_rcrds(
+    async fn _ns_a_rcrds(
         &self,
-        a: Result<&RecordSection<'_, bytes::Bytes>, &ParseError>,
-        b: Result<&RecordSection<'_, bytes::Bytes>, &ParseError>,
-        c: &mut Vec<SocketAddr>,
-    ) -> () {
+        a: &RecordSection<'_, bytes::Bytes>,
+        b: &RecordSection<'_, bytes::Bytes>,
+        c: &mut Vec<SocketAddr>
+    ) -> Result<(), ()> {
 
-        let a_sec = match a {
-            Ok(o) => o.limit_to_in::<Ns<_>>(),
-            Err(e) => panic!("{e}"),
-        };
+        // fuzzing my code so trigger errors on purpose
+        if chaos_monkey("_ns_a_rcrds").await {
+            return Err(());
+        }
 
-        let b_sec = match b {
-            Ok(o) => o.limit_to_in::<A>(),
-            Err(e) => panic!("{e}"),
-        };
+        let athrty_sec = a.limit_to_in::<Ns<_>>();
+        let addtnl_sec = b.limit_to_in::<A>();
 
-        for ns_rcrd_res in a_sec {
+        for ns_rcrd_res in athrty_sec {
             let ns_rcrd = ns_rcrd_res.expect("");
-            for a_rcrd_res in b_sec.clone() {
+            for a_rcrd_res in addtnl_sec.clone() {
                 let a_rcrd = a_rcrd_res.expect("");
                 if ns_rcrd.data().nsdname() == a_rcrd.owner() {
-                    c.push(
-                        SocketAddr::new(a_rcrd.data().addr().try_into().unwrap(), 53)
-                    );
-
+                    c.push(SocketAddr::new(a_rcrd.data().addr().try_into().unwrap(), 53));
                 }
-                
-            } 
+            }
         }
-        
+        return if c.len() > 0 { Ok(()) } else { Err(()) };
     }
 
     async fn _get_rcrds(
         &self,
         nhq: NhQuery
-    ) -> Vec<Record<ParsedName<bytes::Bytes>, domain::rdata::A>> {
+    ) -> Result<Vec<Record<ParsedName<bytes::Bytes>, domain::rdata::A>>, ()> {
+
+        // fuzzing my code so trigger errors on purpose
+        if chaos_monkey("_get_rcrds").await {
+            return Err(());
+        }
 
         for ns in nhq.ns {
             println!("query {} {} @{:?}", nhq.na, nhq.rt, ns);
             let rspns = self._get_msg(nhq.na.clone(), nhq.rt, ns).await;
 
+            // prepare all sections (answer, authority & additional)
+            let answr_sctn = rspns.answer().expect("Answer Section failed to parse!");
+            let athrty_sctn = rspns.authority().expect("Authority Section failed to parse!");
+            let addtnl_sctn = rspns.additional().expect("Additional Section failed to parse!");
+
             // check if contains answer
             if rspns.header_counts().ancount() > 0 {
-                println!("answer");
                 let mut rslt_rcrds: Vec<
                     Record<ParsedName<bytes::Bytes>, domain::rdata::A>
                 > = vec![];
 
                 // extract records from section into vector
-                self._sctn_rcrds(rspns.answer().as_ref(), &mut rslt_rcrds).await;
-
-                return rslt_rcrds;
+                if self._sctn_rcrds(&answr_sctn, &mut rslt_rcrds).await.is_ok() {
+                    println!("RESULT {:?}", rslt_rcrds);
+                    return Ok(rslt_rcrds);
+                } else {
+                    return Err(());
+                }
             }
 
             if rspns.header_counts().nscount() > 0 {
                 println!("authority");
-                let ns_auth_sec = match rspns.authority() {
-                    Ok(o) => o.limit_to_in::<Ns<_>>(),
-                    Err(e) => panic!("{e}"),
-                };
 
-                for ns_rcrd_res in ns_auth_sec {
-                    let ns_rcrd = match ns_rcrd_res {
-                        Ok(o) => o,
-                        Err(e) => {
-                            eprintln!("RECORD PANICED, skipping this record {}", e);
-                            continue;
+                let mut nxt_auth_ns: Vec<SocketAddr> = vec![];
+                if rspns.header_counts().adcount() > 0 {
+                    match self._ns_a_rcrds(&athrty_sctn, &addtnl_sctn, &mut nxt_auth_ns).await {
+                        Ok(_) => (),
+                        Err(_) => {
+                            eprintln!("Extracting Nameserver with Glue was not possible.");
                         }
-                    };
-
-                    println!("{:?}", ns_rcrd.data());
-
-                    // start looking for ip of this nameserver
-                    // now searching for a new authority to send the next request to.
-                    let mut nxt_auth_ns: Vec<SocketAddr> = vec![];
-
-                    if rspns.header_counts().adcount() > 0 {
-
-                        self._ns_rcrds(rspns.authority().as_ref(), rspns.additional().as_ref(), &mut nxt_auth_ns).await;
-
-                        // let a_add_sec = match rspns.additional() {
-                        //     Ok(o) => o.limit_to_in::<A>(),
-                        //     Err(e) => panic!("{e}"),
-                        // };
-                        // for a_rcrd_res in a_add_sec {
-                        //     let a_rcrd = match a_rcrd_res {
-                        //         Ok(o) => o,
-                        //         Err(e) => {
-                        //             eprintln!("RECORD PANICED, skipping this record {}", e);
-                        //             continue;
-                        //         }
-                        //     };
-
-                        //     if a_rcrd.owner() == ns_rcrd.data().nsdname() {
-                        //         println!(
-                        //             "found address ({}) for {}",
-                        //             a_rcrd.data().addr(),
-                        //             ns_rcrd.data().nsdname()
-                        //         );
-                        //         nxt_auth_ns.push(
-                        //             SocketAddr::new(a_rcrd.data().addr().try_into().unwrap(), 53)
-                        //         );
-                        //     }
-                        // }
                     }
-
+                }
+                let mut ns_iterator = athrty_sctn.limit_to_in::<Ns<_>>();
+                loop {
                     if nxt_auth_ns.len() == 0 {
-                        // f***ing useless additional section
+                        let x = match ns_iterator.next() {
+                            Some(s) => {
+                                println!("next ns is {:?}", s);
+                                s
+                            },
+                            None => {
+                                break;
+                            }
+                        };
+
+                        let y = x.expect("");
                         let a_rcrd_q = NhQuery::new(
-                            ns_rcrd.data().nsdname().to_name(),
+                            y.data().nsdname().to_name(),
                             Rtype::A,
                             None
                         ).await;
-                        let a_rcrd_rspns = Box::pin(self._get_rcrds(a_rcrd_q)).await;
+
+                        let a_rcrd_rspns = match Box::pin(self._get_rcrds(a_rcrd_q)).await {
+                            Ok(o) => o,
+                            Err(_) => {
+                                eprintln!("Failed to query ip for nameserver {}", y.data().nsdname());
+                                continue;
+                            }
+                        };
+
                         for a_rcrd in a_rcrd_rspns {
                             nxt_auth_ns.push(
                                 SocketAddr::new(a_rcrd.data().addr().try_into().unwrap(), 53)
                             );
                         }
                     }
-
-                    println!(
-                        "created list of ips ({:?}) for {:?} | {:?}",
-                        nxt_auth_ns,
-                        ns_rcrd.data().nsdname(),
-                        rspns.authority().expect("").next().expect("").expect("").owner()
-                    );
-
                     // create new request
                     let q = NhQuery::new(nhq.na.clone(), nhq.rt, Some(nxt_auth_ns)).await;
 
                     return Box::pin(self._get_rcrds(q)).await;
                 }
             }
-
-            break;
         }
-
-        return vec![];
+        eprintln!("WHAT THE FUCK WHY AM I HERE");
+        return Err(());
     }
 
     async fn query(&self, nhq: NhQuery) -> String {
-        self._get_rcrds(nhq).await;
+        match self._get_rcrds(nhq.clone()).await {
+            Ok(o) => {
+                println!("{:?}", o);
+            }
+            Err(e) => println!("Resultion for {} failed: {:?}", nhq.na, e),
+        }
         "wip".to_string()
     }
 
@@ -303,7 +297,7 @@ async fn main() {
     println!("{:?}", r);
 
     let q = NhQuery::new(
-        domain::base::Name::<Vec<u8>>::from_str("example.com.").unwrap(),
+        domain::base::Name::<Vec<u8>>::from_str("akamai.com.").unwrap(),
         domain::base::Rtype::A,
         None
     ).await;
